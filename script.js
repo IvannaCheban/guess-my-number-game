1f29ff7b1504a8dffcd0a895a0e2fd6b85556d14
IvannaCheban <ivanna_cheban@ukr.net>
2023-05-04 11:52:10 +0300

    add the query seclector

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-'use strict';
+"use strict";
+console.log(document.querySelector(".message").textContent); //the query seclector is a method available on document object
+//the message is a class selector that we reffer to in html //this first part selects the element and then on that
+//element we can read the text content property using another dot
+//
